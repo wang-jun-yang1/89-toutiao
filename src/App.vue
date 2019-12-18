eb729c94f3a074562dd1492d23913e03c56f6c75
unknown <wangjunyang@look.com>
2019-12-18 17:54:11 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <!-- 一级路由容器 -->
-    <router-view/>
-  </div>
+   <router-view></router-view>
 </template>
 
 <style lang="less">
