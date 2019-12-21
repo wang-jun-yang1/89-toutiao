1e47647a0508a49d46b6c3f1dffdd91b7186f8b8
unknown <wangjunyang@look.com>
2019-12-21 20:29:05 +0800

    面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import layoutAside from './home/layout-aside'
 import layoutHeader from './layout-header'
+import Breadcrumb from './common/bread-crumb'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)// 全剧注册
     Vue.component('layout-header', layoutHeader)// 全剧注册
+    Vue.compoment('bread-crumb', Breadcrumb)// 注册一个全局面包屑组件
   }
 }
