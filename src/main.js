003078c820a3344cfd8cb13f16042ecc6f5b6102
unknown <wangjunyang@look.com>
2019-12-21 16:23:09 +0800

    主页模块-导航守卫

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import 'element-ui/lib/theme-chalk/index.css' // 引入element样式
 import './styles/index.less' // 引入初始化样式
 import axios from 'axios'
 import Component from './components'// 引入组件对象
-
+import './permision'// 引入js
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'// 赋值黑马头条的默认地址
 Vue.prototype.$axios = axios// 赋值给全局对象
 Vue.config.productionTip = false
