755b242bcc86d1a5aa1d08c70050ea0e5f6a686c
unknown <wangjunyang@look.com>
2019-12-27 20:39:28 +0800

    文章列表-获取搜索栏值

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -9,13 +9,15 @@
            <span>文章状态</span>
           </el-col>
           <el-col :span="18">
+              <!-- 0-草稿 1-待审核 2-审核通过 3-审核失败  不传为全部 -->
               <!-- 单选框组 -->
-            <el-radio-group>
-              <el-radio>全部</el-radio>
-              <el-radio>草稿</el-radio>
-              <el-radio>待审核</el-radio>
-              <el-radio>审核失败</el-radio>
-              <el-radio>审核通过</el-radio>
+            <el-radio-group v-model="formData.status">
+                <!-- 全部这个5是默认的，在传参的时候判断一下 是不是5 如果是 就传个null -->
+              <el-radio :label="5">全部</el-radio>
+              <el-radio :label="0">草稿</el-radio>
+              <el-radio :label="1">待审核</el-radio>
+              <el-radio :label="3">审核失败</el-radio>
+              <el-radio :label="2">审核通过</el-radio>
             </el-radio-group>
           </el-col>
 
@@ -25,7 +27,12 @@
               <span>频道列表</span>
           </el-col>
           <el-col :span="18">
-            <el-select></el-select>
+            <el-select v-model="formData.channel_id">
+                      <!-- 循环生成多个el-option
+              lable指的是el-option显示值
+              value指的是 el-option的存储值-->
+                <el-option  v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
+            </el-select>
           </el-col>
       </el-row>
       <el-row class="searchtool">
@@ -34,10 +41,12 @@
           </el-col>
             <el-col :span="18">
              <el-date-picker
+               v-model="formData.dateRange"
              type="daterange"
              range-separator="-"
              start-placeholder="开始日期"
-             end-placeholder="结束日期">
+             end-placeholder="结束日期"
+            >
              </el-date-picker>
           </el-col>
       </el-row>
@@ -46,7 +55,29 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      formData: {
+        status: 5, // 状态
+        channel_id: null, // 默认是空
+        dateRange: []
+      },
+      channels: [] // 定义一个channels接收频道
+    }
+  },
+  methods: {
+    // 获取频道
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels // 获取频道数据
+      })
+    }
+  },
+  created () {
+    this.getChannels()// 调用获取频道数据
+  }
 }
 </script>
 
