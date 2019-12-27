7fea4a4376339a281f55af98d1e4a21d2c8f943b
unknown <wangjunyang@look.com>
2019-12-27 13:19:29 +0800

    素材列表-获取收藏

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -5,7 +5,7 @@
       <template slot="title">素材管理</template>
       </bread-crumb>
       <!-- 素材 -->
-      <el-tabs v-model="activeName">
+      <el-tabs  v-model="activeName" @tab-click="changeTab">
           <el-tab-pane label="全部素材" name="all">
               <!-- 全部素材内容 -->
               <div class="img-list">
@@ -20,6 +20,11 @@
           </el-tab-pane>
           <el-tab-pane label="收藏素材" name="collect">
               <!-- 收藏素材内容 -->
+              <div class="img-list">
+                  <el-card class="img-card" v-for="item in list" :key='item.id'>
+                      <img :src="item.url" alt="">
+                  </el-card>
+              </div>
           </el-tab-pane>
       </el-tabs>
   </el-card>
@@ -34,17 +39,22 @@ export default {
     }
   },
   methods: {
+    changeTab () {
+      this.getAllMaterial()// 获取所有素材
+    },
+    // 切换tab事件
     getAllMaterial () {
       this.$axios({
         url: '/user/images',
-        parmas: { collect: false }
+        params: { collect: this.activeName === 'collect' }
       }).then(result => {
         this.list = result.data.results
       })
     }
+
   },
   created () {
-    this.getAllMaterial()
+    this.getAllMaterial()// 第一次进入activeName 为all 实际上是查的全部的数据
   }
 }
 </script>
