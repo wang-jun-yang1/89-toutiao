f4fc56992c89ca49e017b3370dea0c0f1f8faf49
unknown <wangjunyang@look.com>
2019-12-27 17:35:24 +0800

    素材列表-收藏和删除

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -17,8 +17,9 @@
                   <el-card class="img-card" v-for="item in list" :key='item.id'>
                       <img :src="item.url" alt="">
                     <el-row class="operate" type="flex" justify="space-around">
-                        <i class="el-icon-star-on"></i>
-                        <i class="el-icon-delete-solid"></i>
+                      <!-- v-bind:style 根据收藏状态显示图标颜色-->
+                        <i @click="collectOrCancel(item)" :style="{color:item.is_collected ? 'red' : ''}" class="el-icon-star-on"></i>
+                        <i @click="delMaterial(item.id)" class="el-icon-delete-solid"></i>
                     </el-row>
                   </el-card>
               </div>
@@ -70,6 +71,34 @@ export default {
     }
   },
   methods: {
+    // 删除
+    delMaterial (id) {
+      this.$confirm('确定要删除？').then(() => {
+      // 只有点击了确定才会执行
+      // 调用删除接口
+        this.$axios({
+          url: `/user/images/${id}`,
+          method: 'delete'
+        }).then(() => {
+          // 重新拉取
+          this.getAllMaterial()// 重新加载
+        })
+      })
+    },
+    // 收藏或者取消收藏
+    collectOrCancel (row) {
+      // 调用 收藏或者取消接口
+      this.$axios({
+        url: `/user/images/${row.id}`,
+        method: 'put',
+        data: {
+          collect: !row.is_collected // 状态取反 收藏-取消 取消-收藏
+        }
+      }).then(() => {
+        // 成功进入到then
+        this.getAllMaterial()// 重新加载
+      })
+    },
     // 上传图片
     uploadImg (params) {
       this.loading = true // 打开进度条
@@ -135,6 +164,9 @@ export default {
           left: 0;
           background-color: #f4f5f6;
           height: 30px;
+         i{
+           cursor: pointer;
+         }
         }
     }
 }
